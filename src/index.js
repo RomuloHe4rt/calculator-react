4bd8ddcb5f355fcf5556bedc656b16c3ceb9ca25
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-30 18:42:53 -0300

    style: inserting button style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Calculator from "./main/Calculator";
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Calculator from "./main/Calculator"
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <div>
     <h1>Calculadora</h1>
     <Calculator />
   </div>, 
-  document.getElementById('root'));
-reportWebVitals();
+  document.getElementById('root'))
+reportWebVitals()
